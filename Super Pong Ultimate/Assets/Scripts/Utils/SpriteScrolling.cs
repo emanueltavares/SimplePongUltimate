07de4a21d420154147aab84f35e629490895a18e
Emanuel Tavares <emanuelmaues@gmail.com>
2020-02-25 17:01:41 -0300

    Improve Sprite Scrolling

---
+++ Super Pong Ultimate/Assets/Scripts/Utils/SpriteScrolling.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Application.Utils
+{
+
+    public class SpriteScrolling : MonoBehaviour
+    {
+        [SerializeField] private float _maxTimeToScaleWidth;
+        [SerializeField] private float _maxTimeToScaleHeight;
+
+        private SpriteRenderer _spriteRenderer;
+        private Vector2 _minSize;
+        private Vector2 _maxSize;
+
+        protected virtual void OnEnable()
+        {
+            if (_spriteRenderer == null)
+            {
+                _spriteRenderer = GetComponent<SpriteRenderer>();
+            }
+
+            _minSize = _spriteRenderer.size;
+            _maxSize = _minSize * 2f;
+        }
+
+        protected virtual void Update()
+        {
+            Vector2 size = _spriteRenderer.size;
+            float t;
+            if (_maxTimeToScaleWidth > 0)
+            {
+                t = (Time.time % _maxTimeToScaleWidth) / _maxTimeToScaleWidth;
+                size.x = Mathf.Lerp(_minSize.x, _maxSize.x, t);
+            }
+            if (_maxTimeToScaleHeight > 0)
+            {
+                t = (Time.time % _maxTimeToScaleHeight) / _maxTimeToScaleHeight;
+                size.y = Mathf.Lerp(_minSize.y, _maxSize.y, t);
+            }
+            _spriteRenderer.size = size;
+        }
+    }
+}
