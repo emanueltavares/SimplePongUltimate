5a00d6782d9c0012bd9612fa188f606dfdaf6758
Emanuel Tavares <emanuelmaues@gmail.com>
2020-02-25 23:06:22 -0300

    Add basic AI to the game

---
+++ Super Pong Ultimate/Assets/Scripts/Controllers/AIPaddleController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Utils;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Application.Controllers
+{
+    public class AIPaddleController : MonoBehaviour
+    {
+        [SerializeField] private Transform _ballTransform;
+        [SerializeField] private float _maxSpeed = 5;
+
+        private Rigidbody2D _rigidbody2D;
+
+        protected virtual void OnEnable()
+        {
+            if (_rigidbody2D == null)
+            {
+                _rigidbody2D = GetComponent<Rigidbody2D>();
+            }
+        }
+
+        protected virtual void Update()
+        {
+            float nextPositionY = Mathf.MoveTowards(_rigidbody2D.position.y, _ballTransform.position.y, _maxSpeed * Time.deltaTime);
+            Vector2 nextPosition = new Vector2(_rigidbody2D.position.x, nextPositionY);
+            _rigidbody2D.MovePosition(nextPosition);
+        }
+    }
+}
